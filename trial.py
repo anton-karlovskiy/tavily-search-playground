b06ef4f94654c8a6f768286c06fa96d1934e1e69
Anton K. <antonkarlovskiy@outlook.com>
2024-08-13 15:42:37 +0200

    feat: add exception handling codes

--- trial.py
+++ trial.py
@@ -1,4 +1,4 @@
-from tavily import TavilyClient
+from tavily import TavilyClient, MissingAPIKeyError, InvalidAPIKeyError, UsageLimitExceededError
 import os
 from dotenv import load_dotenv
 
@@ -7,15 +7,25 @@ load_dotenv()
 TAVILY_API_KEY = os.getenv('TAVILY_API_KEY')
 
 # Step 1. Instantiating your TavilyClient
-tavily_client = TavilyClient(api_key=TAVILY_API_KEY)
 
-# Step 2. Executing a simple search query
-response = tavily_client.search(
-  "How to forecast potential fishing zones?",
-  max_results=50,
-  topic="general",
-  include_domains=["https://link.springer.com"]
-)
+try:
+    tavily_client = TavilyClient(api_key=TAVILY_API_KEY)
+except MissingAPIKeyError:
+    print("API key is missing. Please provide a valid API key.")
+
+# Step 2. Executing a search query
+try:
+    response = tavily_client.search(
+        "How to forecast potential fishing zones?",
+        max_results=2000,
+        topic="general",
+        include_domains=["https://link.springer.com"]
+    )
+except InvalidAPIKeyError:
+    print("Invalid API key provided. Please check your API key.")
+except UsageLimitExceededError:
+    print("Usage limit exceeded. Please check your plan's usage limits or consider upgrading.")
+
 
 # Step 3. That's it! You've done a Tavily Search!
 print(response)
